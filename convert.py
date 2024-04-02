539eb3d921860ce32108d894d9b6292a841a764c
SymphonyWang <symphonywang2020@gmail.com>
2024-04-02 16:44:59 -0400

    Ver2.0
    functions mortify

--- convert.py
+++ convert.py
@@ -1,18 +1,56 @@
-import os
+import pandas as pd
 import subprocess
-import csv
+import os
 
-path = "/home/xiw4013/mAb3D/input"
-inputlist = "/home/xiw4013/mAb3D/inputlist.csv"
+ab3d_sheet = "/home/xiw4013/mAb3D/Ab3D-E-Screening - Sheet1.csv"
+inputlist = "/home/xiw4013/mAb3D/inputlistV2.csv"
+input_dir = "/home/xiw4013/mAb3D/input"
 output_dir = "/home/xiw4013/mAb3D/zarr_upload/"
 
-# def Convert czi to ome-zarr using bioformats2raw
-def convert_to_ome_zarr(input_path, output_path):    
+# Read the CSV file
+df = pd.read_csv(ab3d_sheet)
 
-    # Run the bioformats2raw command
-    # command = ['bioformats2raw', input_path, output_path, '--compression', 'zlib', '--compression-properties', 'level=9', '--dimension-order', 'XYZCT', '--resolutions', '3']
-    # command = ['bioformats2raw', input_path, output_path, '--compression', 'zlib', '--compression-properties', 'level=9', '--dimension-order', 'XYCZT', '--resolutions', '3']
-    # command = ['bioformats2raw', input_path, output_path, '--compression', 'zlib', '--compression-properties', 'level=9', '--dimension-order', 'XYTZC', '--resolutions', '3']
+# Define the mapping of columns to section and exposure
+mapping = {
+    'Ms1 Result': '0',
+    'Ms2 Result': '1',
+    'Hu1 (JC7) Result': '2',
+    'Hu2 (JC8) Result': '3',
+    'Hu3 (JC9) Result': '4',
+    'Gut1 Result': '5',
+    'Gut2 Result': '6',
+}
+
+# Create a new DataFrame with the required columns
+new_df = pd.DataFrame(columns=['filename', 'section', 'exposure', 'transferflag', 'uploadflag'])
+
+# Iterate over the rows of the DataFrame
+for _, row in df.iterrows():
+    for column, section in mapping.items():
+        exposure = row[column]
+        if pd.notna(exposure) and exposure.startswith('Exp'):
+            exposure_number = exposure[3:]  # Remove 'Exp' prefix
+            new_row = {
+                'filename': row['Czi Name'],
+                'section': section,
+                'exposure': exposure_number,
+                'transferflag': '0',
+                'uploadflag': '0',
+                'shortname': None,
+                'orgURL': None,
+            }
+            new_df = new_df._append(new_row, ignore_index=True)
+
+# Set each column as str type
+new_df = new_df.astype(str)
+
+# Write the new DataFrame to a CSV file
+new_df.to_csv(inputlist, index=False)
+print(f"Input Items List was Saved the new DataFrame to {inputlist}")
+
+# def Convert czi to ome-zarr using bioformats2raw
+def convert_to_ome_zarr(input_path, output_path): 
+    
     # dimension-order Default ('XYZCT')
     command = ['bioformats2raw', input_path, output_path, '--compression', 'zlib', '--compression-properties', 'level=9', '--resolutions', '5']
     result = subprocess.run(command, stderr=subprocess.PIPE)
@@ -24,51 +62,52 @@ def convert_to_ome_zarr(input_path, output_path):
     # If there was an error, return False and the error details
         error_detail = result.stderr.decode('utf-8') if result.stderr else "Unknown error"
         return False, error_detail
+    
+def main():
+  # Read the inputlist CSV file
+  inputlist_df = pd.read_csv(inputlist, dtype={'filename': str, 'section': str, 'exposure': str, 'uploadflag': str})
 
+  # Generate a list of .czi files from the inputlist DataFrame,If there are files with the same name, merge them into one
+  czi_files = inputlist_df[inputlist_df['filename'].str.endswith('.czi')]['filename'].unique().tolist()
+  
+  # Iterate over the rows of the inputlist DataFrame
+  for filename in czi_files:
+    # Check if the file exists in the input directory
+    input_path = os.path.join(input_dir, filename)
+    if not os.path.exists(input_path):
+      print(f"File {filename} does not exist in the input directory. Skipping to next file.")
+      continue
 
-# Open the CSV file in write mode
-with open(inputlist, 'w', newline='') as csvfile:
-    # Define the fieldnames
-    fieldnames = ['filename', 'section', 'exposure', 'transferflag', 'uploadflag']
+    # Remove the .czi extension from the base filename
+    base_name = os.path.splitext(filename)[0]
 
-    # Create a CSV DictWriter
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    # Construct the output path
+    output_path = os.path.join(output_dir, f"{base_name}.zarr")
+    
+    # Check if the output directory already exists
+    if os.path.exists(output_path):
+      inputlist_df['transferflag'] = inputlist_df['transferflag'].astype(str)
+      inputlist_df.loc[inputlist_df['filename'] == filename, 'transferflag'] = '2'
+      print(f"Output directory {output_path} already exists. Skipping file {filename}.")
+      continue
+    
+    # Convert the file to OME-Zarr format
+    success, error_detail = convert_to_ome_zarr(input_path, output_path)
+    
+    if not success:
+      inputlist_df['transferflag'] = inputlist_df['transferflag'].astype(str)
+      inputlist_df.loc[inputlist_df['filename'] == filename, 'transferflag'] = 'e'
+      print(f"Error converting file {filename}. Error details: {error_detail}. Skipping to next file.")
+    else:
+      print(f"File {filename} converted successfully.")
+      # When the conversion is successful, update all 'transferflag' under this filename in inputlist_df to '1'
+      inputlist_df['transferflag'] = inputlist_df['transferflag'].astype(str)
+      inputlist_df.loc[inputlist_df['filename'] == filename, 'transferflag'] = '1'
+  
+  # Write the updated inputlist DataFrame to the CSV file
+  inputlist_df.to_csv(inputlist, index=False)
+  
+  print(f"All Files Conversion complete. CSV file written to {inputlist}.")
 
-    # Write the header to the CSV file
-    writer.writeheader()
-
-    # Use os.listdir to get all files in the directory
-    for filename in os.listdir(path):
-        # Use os.path.isfile to check if it's a file (not a directory)
-        if os.path.isfile(os.path.join(path, filename)):
-            # Check if the file has a .czi extension
-            if filename.endswith('.czi'):
-                # Extract the base filename without the path
-                base_filename = os.path.basename(filename)
-
-                # Remove the .czi extension from the base filename
-                base_name = os.path.splitext(base_filename)[0]
-
-                # Construct the input path
-                input_path = os.path.join(path, filename)
-                # Construct the output path
-                output_path = os.path.join(output_dir, f"{base_name}.zarr")
-
-                # Check if the output directory already exists
-                if os.path.exists(output_path):
-                    writer.writerow({'filename': filename, 'transferflag': '2', 'uploadflag': '0'})
-                    print(f"Output directory {output_path} already exists. Skipping file {filename}.")
-                    continue
-
-                # Convert the file to OME-Zarr format
-                success, error_detail = convert_to_ome_zarr(input_path, output_path)
-
-                if not success:
-                  writer.writerow({'filename': filename, 'transferflag': 'e', 'uploadflag': '0'})
-                  print(f"Error converting file {filename}. Error details: {error_detail}. Skipping to next file.")
-                else:
-                    writer.writerow({'filename': filename, 'transferflag': '1', 'uploadflag': '0'})
-                    print(f"Successfully converted file {filename} and wrote to {inputlist}.")
-
-
-print(f"Conversion complete. CSV file written to {inputlist}.")
+# Call the main function
+main()
--- upload.py
+++ upload.py
@@ -1,15 +1,25 @@
 import os
-import csv
 import subprocess
+import pandas as pd
+import shutil
+from urllib.parse import quote
 
 # Define the server name and upload path
 upload_path = '/home/xiw4013/mAb3D/zarr_upload/'
+inputlist = "/home/xiw4013/mAb3D/inputlistV2.csv"
 s3_bucket = 'mAb3D'
-inputlist = "/home/xiw4013/mAb3D/inputlist.csv"
+resolutions = range(5)
+awsbucket_path = 's3://mAb3D/test/'
+zarr_upload_list = "/home/xiw4013/mAb3D/Zarr_upload_list.csv"
 
 # Function to upload zarr file to AWS S3 bucket
-def upload_to_s3(zarr_file, zarr_path):
-    aws_command = ['aws', '--endpoint-url', 'https://redcloud.cac.cornell.edu:8443/', '--no-verify', 's3', '--profile', 'CAC', 'cp',  '--recursive', zarr_path, 's3://mAb3D/test/' + zarr_file]
+def upload_to_s3(zarr_file, section_index):
+    zarrpath = upload_path + zarr_file + '/' + str(section_index) + '/'
+    awspath = awsbucket_path + zarr_file + '/' + str(section_index) + '/'
+    # print(f"zarrpath:~~~~~~~~~~~~ {zarrpath}")
+    # print(f"awspath:~~~~~~~~~~~~ {awspath}")
+
+    aws_command = ['aws', '--endpoint-url', 'https://redcloud.cac.cornell.edu:8443/', '--no-verify', 's3', '--profile', 'CAC', 'cp',  '--recursive', zarrpath, awspath]
     result = subprocess.run(aws_command, stderr=subprocess.PIPE)
 
      # Check if the command was successful
@@ -19,44 +29,104 @@ def upload_to_s3(zarr_file, zarr_path):
     # If there was an error, return False and the error details
         error_detail = result.stderr.decode('utf-8') if result.stderr else "Unknown error"
         return False, error_detail
+    
+# Function to delete a folder and all its contents    
+def delete_folder(folder_path):
+    try:
+        # Use shutil.rmtree() to remove the directory and all its contents
+        shutil.rmtree(folder_path)
+        print(f"The folder '{folder_path}' and all its contents have been successfully deleted.")
+    except Exception as e:
+        print(f"Unable to delete the folder '{folder_path}': {e}")
 
-# Open the CSV file
-with open(inputlist, 'r') as f:
-    reader = csv.DictReader(f)
-    rows = list(reader)
+def create_org_URL (zarr_file, section):
+  try:     
 
-# Open the output CSV file
-with open('ZarrURL.csv', 'w', newline='') as f:
-    writer = csv.DictWriter(f, fieldnames=['filename', 'URL'])
-    writer.writeheader()
+      # encode zarr file URL
+      zarr_file_encode = quote(zarr_file)
+      orgURL = "https://neuroglancer-demo.appspot.com/#!%7B%22dimensions%22:%7B%22x%22:%5B0.0000013759848761314483%2C%22m%22%5D%2C%22y%22:%5B0.0000013759848761314483%2C%22m%22%5D%2C%22z%22:%5B0.0000013759848761314483%2C%22m%22%5D%2C%22t%22:%5B1%2C%22%22%5D%7D%2C%22position%22:%5B6847%2C2872%2C0.5%2C0%5D%2C%22crossSectionScale%22:9.227814895369777%2C%22projectionOrientation%22:%5B-0.3254466950893402%2C0.8991797566413879%2C0.2924036383628845%2C0.007767873350530863%5D%2C%22projectionScale%22:12314.085503119006%2C%22layers%22:%5B%7B%22type%22:%22image%22%2C%22source%22:%22zarr2://https://redcloud.cac.cornell.edu:8443/swift/v1/mAb3D/test/{zarr_file_encode1}/{section1}/%22%2C%22localDimensions%22:%7B%22c%27%22:%5B1%2C%22%22%5D%7D%2C%22localPosition%22:%5B1%5D%2C%22tab%22:%22rendering%22%2C%22shaderControls%22:%7B%22normalized%22:%7B%22range%22:%5B0%2C700%5D%7D%7D%2C%22crossSectionRenderScale%22:0.14126447292866626%2C%22name%22:%22layer0%22%7D%5D%2C%22selectedLayer%22:%7B%22size%22:637%2C%22visible%22:true%2C%22layer%22:%22layer0%22%7D%2C%22layout%22:%22xy%22%7D".format(zarr_file_encode1=zarr_file_encode, section1=section)
 
-    for row in rows:
-        # Check if the file exists, the upload flag is 0, and the transfer flag is 1
-        zarr_path = os.path.join(upload_path, row['filename'].replace('.czi', '.zarr'))
-        print(f"zarr_path ******* {zarr_path}")
-        zarr_file = os.path.basename(zarr_path)
-        print(f"zarr_file ******* {zarr_file}")
+      section_map = {
+          '0': 'Ms1',
+          '1': 'Ms2',
+          '2': 'Hu1',
+          '3': 'Hu2',
+          '4': 'Hu3',
+          '5': 'Gut1',
+          '6': 'Gut2'
+      }
 
-        if os.path.exists(zarr_path) and row['uploadflag'] == '0' and row['transferflag'] in ['1']:
-            try:
-                # Upload the file to the S3 bucket
-                success, error_detail = upload_to_s3(zarr_file, zarr_path)
-                
-                if not success:
-                    raise Exception(f"Error uploading file {row['filename']}: {error_detail}")
-                else:  
-                  print(f"Uploaded file {row['filename']} to S3 bucket {s3_bucket}.")
-                  # Write the filename and URL to the output CSV file
-                  writer.writerow({'filename': row['filename'].replace('.czi', '.zarr'), 'URL': f"<a href='https://redcloud.cac.cornell.edu:8443/{s3_bucket}/test/{row['filename'].replace('.czi', '.zarr')}' target='_blank'>Link</a>"})
-                  # Update the upload flag to 1
-                  row['uploadflag'] = '1'
-            except Exception as e:
-                print(f"Error uploading file {row['filename']}: {e}")
-                # Update the upload flag to 'e'
-                row['uploadflag'] = 'e'
+      shortname = zarr_file.replace('.zarr', '') + '-' + section_map[section]
+      # print(f"shortname     ] {shortname}")
+      # print(f"orgURL     ] {orgURL}")
+      print(f"creating org URL for {shortname} have been successfully completed.")
+      return shortname, orgURL
+  except Exception as e:
+      print(f"Error creating org URL: {e}")
+      return None, None
 
-# Write the updated rows back to the input CSV file
-with open(inputlist, 'w', newline='') as f:
-    writer = csv.DictWriter(f, fieldnames=rows[0].keys())
-    writer.writeheader()
-    writer.writerows(rows)
+# Read the inputlist.csv using pandas
+df = pd.read_csv(inputlist, dtype={'filename': str, 'section': str, 'exposure': str, 'uploadflag': str, 'orgURL': str, 'shortname': str})
+print(f"df {df}")
+
+# open a new csv file to write the updated information using pandas
+# df[['filename', 'section']].to_csv(zarr_upload_list, index=False)
+
+# Traverse filename in upload path
+for index, row in df.iterrows():
+  filename = row['filename']
+  section = row['section']
+  exposure = row['exposure']
+  uploadflag = row['uploadflag']
+  shortname = row['shortname']
+  orgURL = row['orgURL']
+  
+  # Define the path to the zarr file
+  zarr_path = os.path.join(upload_path, filename.replace('.czi', '.zarr'))
+  zarr_file = os.path.basename(zarr_path)
+  print(f"zarr_path ******* {zarr_path}")
+  print(f"zarr_file ******* {zarr_file}")
+
+  # Check if the file exists, the upload flag is f
+  if uploadflag == '0' and os.path.exists(zarr_path):
+    try:
+      # total 4 exposures for each section: 0, 1, 2, 3
+      # 0 : Green channel 1: Red channel 2: Far Red channel Exp1 3: Far Red channel Exp2
+      # Here only consider Far Red channel
+      exposure_delete_ranges = {
+        # '1': [0, 1, 3],
+        # '2': [0, 1, 2]   
+        '1': [0, 2],
+        '2': [0, 1]   
+      }
+      # loop through the resolutions,exposure_delete_ranges and delete the folders
+      for resolution in resolutions:              
+          for exposure_delete in exposure_delete_ranges.get(exposure, []):
+            delete_path = os.path.join(upload_path, zarr_file, section, str(resolution), '0', str(exposure_delete))
+            print(f"delete_path++++++++++: {delete_path}")
+            delete_folder(delete_path)
+
+      # Upload the file to the S3 bucket
+      success, error_detail = upload_to_s3(zarr_file, section)
+
+      if not success:
+        raise Exception(f"Error uploading file {filename}: {error_detail}")
+      else:  
+        print(f"Uploaded file {filename} to S3 bucket {s3_bucket}.")
+        # Update the upload flag to 1
+        df.at[index, 'uploadflag'] = '1'
+        df.to_csv(inputlist, index=False)
+
+        shortname, orgURL = create_org_URL(zarr_file, section)
+        df.at[index, 'shortname'] = shortname  # Updating shortname in DataFrame
+        df.at[index, 'orgURL'] = orgURL  # Updating orgURL in DataFrame
+
+    except Exception as e:
+        print(f"Error uploading file {filename}: {e}")
+        # Update the upload flag to 'e'
+        df.at[index, 'uploadflag'] = 'e'
+
+  # Write the updated DataFrame back to the input CSV file    
+  df.to_csv(inputlist, index=False)
+
+print(f"Updated inputlist CSV file written to {inputlist}.")
